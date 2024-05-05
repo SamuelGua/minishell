c82c4c2ab00067df8446d1c03d6509c33cc08a58
SamuelGua <samuel.cely@hotmail.fr>
2024-05-05 19:44:05 +0200

    git make fast

--- src/minishell/main.c
+++ src/minishell/main.c
@@ -19,58 +19,9 @@ void 	print_message(char *str1, char *str2, char *str3, int fd)
 	ft_putstr_fd(str3, fd);
 }
 
-// int main(int ac, char **av, char **envp)
-// {
-// 	((void)ac, (void)av);
-// 	t_exec				exec;
-// 	struct sigaction	c_signal;
-// 	struct sigaction	slash_signal;
-
-// 	exec.env = init_env(envp);
-//  	exec.export = init_export(exec.env);
-
-// 	sig_c_interactive(&c_signal);
-// 	sig_slash_interactive(&slash_signal);
-// 	exec.error_code = 0;
-// 	while (1)
-// 	{
-// 		sigaction(SIGINT, &c_signal, NULL);
-// 		// sigaction(SIGQUIT, &d_signal, NULL);
-// 		sigaction(SIGQUIT, &slash_signal, NULL);
-// 		exec.prompt = readline("\002minishell > \002");
-// 		if (!exec.prompt)
-// 			return (ft_free_env(exec.env), ft_free_export(exec.export), 2);
-// 		exec.token = init_token(exec.prompt, exec.token);
-// 		if (!exec.token)
-// 		{
-// 			free(exec.prompt);
-// 			continue;
-// 		}
-// 		(add_history(exec.prompt), free(exec.prompt));
-// 		if (is_valid_token(exec.token))
-// 		{
-// 			exec.error_code = 2;
-// 			continue;
-// 		}
-// 		exec.cmds = build_cmd(&exec);
-// 		if (!exec.cmds && errno == ENOMEM)
-// 			return (2);
-// 		else if (!exec.cmds)
-// 		{
-// 			exec.error_code = 0;
-// 			continue ;
-// 		}
-// 		exec.error_code	= execution(&exec);
-// 	}
-// 	return (0);
-// }
-
-int	main(int argc, char **argv, char **envp)
+int main(int ac, char **av, char **envp)
 {
-	char	**arg_input;
-	int		i;
-
-	((void)argc, (void)argv);
+	((void)ac, (void)av);
 	t_exec				exec;
 	struct sigaction	c_signal;
 	struct sigaction	slash_signal;
@@ -81,81 +32,36 @@ int	main(int argc, char **argv, char **envp)
 	sig_c_interactive(&c_signal);
 	sig_slash_interactive(&slash_signal);
 	exec.error_code = 0;
-
-
-	if (argc == 3 && ft_strcmp(argv[1], "-c") == 0 && argv[2])
+	while (1)
 	{
-		arg_input = ft_split(argv[2], ';');
-		if (!arg_input)
-			// exit
-		i = 0;
-		while (arg_input[i])
+		sigaction(SIGINT, &c_signal, NULL);
+		// sigaction(SIGQUIT, &d_signal, NULL);
+		sigaction(SIGQUIT, &slash_signal, NULL);
+		exec.prompt = readline("\002minishell > \002");
+		if (!exec.prompt)
+			return (ft_free_env(exec.env), ft_free_export(exec.export), 2);
+		exec.token = init_token(exec.prompt, exec.token);
+		if (!exec.token)
 		{
-
-			sigaction(SIGINT, &c_signal, NULL);
-			// sigaction(SIGQUIT, &d_signal, NULL);
-			sigaction(SIGQUIT, &slash_signal, NULL);
-			exec.prompt = ft_strdup(arg_input[i]);
-			if (!exec.prompt)
-				return (ft_free_env(exec.env), ft_free_export(exec.export), 2);
-			exec.token = init_token(exec.prompt, exec.token);
-			if (!exec.token)
-			{
-				free(exec.prompt);
-				continue;
-			}
-			(add_history(exec.prompt), free(exec.prompt));
-			if (is_valid_token(exec.token))
-			{
-				exec.error_code = 2;
-				continue;
-			}
-			exec.cmds = build_cmd(&exec);
-			if (!exec.cmds && errno == ENOMEM)
-				return (2);
-			else if (!exec.cmds)
-			{
-				exec.error_code = 0;
-				continue ;
-			}
-			exec.error_code	= execution(&exec);
-			i++;
+			free(exec.prompt);
+			continue;
 		}
-	}
-	else
-	{
-		while (1)
+		(add_history(exec.prompt), free(exec.prompt));
+		if (is_valid_token(exec.token))
 		{
-			sigaction(SIGINT, &c_signal, NULL);
-			// sigaction(SIGQUIT, &d_signal, NULL);
-			sigaction(SIGQUIT, &slash_signal, NULL);
-			exec.prompt = readline("\002minishell > \002");
-			if (!exec.prompt)
-				return (ft_free_env(exec.env), ft_free_export(exec.export), 2);
-			exec.token = init_token(exec.prompt, exec.token);
-			if (!exec.token)
-			{
-				free(exec.prompt);
-				continue;
-			}
-			(add_history(exec.prompt), free(exec.prompt));
-			if (is_valid_token(exec.token))
-			{
-				exec.error_code = 2;
-				continue;
-			}
-			exec.cmds = build_cmd(&exec);
-			if (!exec.cmds && errno == ENOMEM)
-				return (2);
-			else if (!exec.cmds)
-			{
-				exec.error_code = 0;
-				continue ;
-			}
-			exec.error_code	= execution(&exec);
+			exec.error_code = 2;
+			continue;
 		}
+		exec.cmds = build_cmd(&exec);
+		if (!exec.cmds && errno == ENOMEM)
+			return (2);
+		else if (!exec.cmds)
+		{
+			exec.error_code = 0;
+			continue ;
+		}
+		exec.error_code	= execution(&exec);
 	}
-		return (0);
-	// Free data and exit minishell when done
+	return (0);
 }
 
